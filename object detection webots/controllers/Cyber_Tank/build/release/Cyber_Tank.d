aa1e53e388b6951087891ae0b88e6b99036585ae
Gowtham <gowtham.sridher2@gmail.com>
2020-07-07 12:52:31 +0530

    Delete Cyber_Tank.d

--- object detection webots/controllers/Cyber_Tank/build/release/Cyber_Tank.d
+++
@@ -1,8 +0,0 @@
-build/release/Cyber_Tank.o: Cyber_Tank.cpp \
- C:\Users\Evil\ Mind\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Users\Evil\ Mind\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Users/Evil\ Mind/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
- C:\Users\Evil\ Mind\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Users\Evil\ Mind\AppData\Local\Programs\Webots/include/controller/cpp/webots/Keyboard.hpp \
- C:\Users\Evil\ Mind\AppData\Local\Programs\Webots/include/controller/cpp/webots/Camera.hpp \
- C:/Users/Evil\ Mind/AppData/Local/Programs/Webots/include/controller/c/webots/camera_recognition_object.h
